e5d37cfb40273e11e2d38ceec164f5c1cf6c66d3
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-12 06:59:44 -0700

    src

---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="products">
+          {this.props.products.map((item) => {
+            let { _id, image, title, price } = item;
+
+            return (
+              <li key={_id}>
+                <div className="product">
+                  <a href={"#" + _id}>
+                    <img src={image} alt={title}></img>
+                    <p>{title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div>{price}</div>
+                    <button className="button primary" type="button">
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
