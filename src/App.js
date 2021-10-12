b7388d1d07e0b42822308a0fe9ab4bfe56121fd7
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-12 10:09:03 -0700

    Filter.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,21 @@ class App extends React.Component {
       sort: "",
     };
   }
-  sortProducts(sort) {}
-  sizeProducts(size) {}
+
+  sortProducts(event) {}
+
+  filterProducts(event) {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  }
   render() {
     return (
       <div className="grid-container">
@@ -27,7 +40,7 @@ class App extends React.Component {
                 count={this.state.products.length}
                 size={this.state.size}
                 sort={this.state.sort}
-                sortProducts={this.sortProducts}
+                filterProducts={this.filterProducts}
                 sizeProducts={this.sizeProducts}
               />
               <Products products={this.state.products}></Products>
